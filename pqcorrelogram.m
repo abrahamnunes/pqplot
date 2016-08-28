eeaca5f9c96594acb40aa74c98dc0aa8a303570b
abrahamnunes <abrahamjnunes@gmail.com>
2016-08-28 13:32:46 -0300

    Added ability to simply plot matrix values

--- pqcorrelogram.m
+++ pqcorrelogram.m
@@ -1,10 +1,50 @@
-function g = pqcorrelogram(x, xlabels)
+function g = pqcorrelogram(x, xlabels, varargin)
 
     load viridis.mat
 
-    [corrs, cpvals] = corr(x);
+    if nargin > 2
+        if ~isempty(varargin{1})
+            if strcmp(varargin{1}, 'corr')
+                [corrs, cpvals] = corr(x);
+                if min(min(corrs)) < 0
+                    colormin = -1.;
+                else
+                    colormin = 0.;
+                end
 
-    im = imagesc(flipud(corrs), [0. 1.]);
+                im = imagesc(flipud(corrs), [colormin 1.]);
+            elseif strcmp(varargin{1}, 'identity')
+                im = imagesc(flipud(x), [0. 1.]);
+            else
+                error('Variable "stat" must either take values "corr" or "identity".');
+            end
+        else
+            [corrs, cpvals] = corr(x);
+            if min(min(corrs)) < 0
+                colormin = -1.;
+            else
+                colormin = 0.;
+            end
+
+            im = imagesc(flipud(corrs), [colormin 1.]);
+        end
+
+        if ~isempty(varargin{2})
+            ylabels = flip(varargin{2});
+        else
+            ylabels = flip(xlabels);
+        end
+    else
+        [corrs, cpvals] = corr(x);
+        if min(min(corrs)) < 0
+            colormin = -1.;
+        else
+            colormin = 0.;
+        end
+
+        im = imagesc(flipud(corrs), [colormin 1.]);
+    end
+
     colormap(viridi);
     cb = colorbar;
     set(cb               , ...
@@ -15,7 +55,7 @@ function g = pqcorrelogram(x, xlabels)
         'FontSize'  , 14              , ...
         'Box'       , 'on'            , ...
         'XTickLabel', xlabels         , ...
-        'YTickLabel', flip(xlabels)   , ...
+        'YTickLabel', ylabels         , ...
         'TickLength', [0. 0.]         , ...
         'LineWidth' , 1.5             );
 
